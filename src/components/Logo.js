eccedd9c99dba9947cb4b3284aa7cf75fbc79be5
AngelMondragon <angel.mondragon@4n.com.mx>
2023-06-23 10:30:42 -0600

    logo

---
+++ src/components/Logo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const styles =  {
+    logo:{
+        fontWeight: '700',
+        fontSize: '2rem'
+    }
+}
+
+
+export default function Logo () {
+    return(
+        <div style={styles.logo}>
+            Shop
+        </div>
+    )
+}
